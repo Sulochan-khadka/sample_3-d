569c5c7b86d3353512bd82bc425987aa64b21232
sulochan <sulochankhadka25@gmail.com>
2024-02-16 03:01:46 +0530

    initial-position

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { Model } from './components/Model';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { useRef } from 'react';
 
 function App() {
+  const cameraRef = useRef();
   return (
     <Canvas>
+      <PerspectiveCamera
+        makeDefault
+        ref={cameraRef}
+        position={[0, 5, 20]} // Adjust the z-coordinate to move the camera closer or farther from the scene
+      />
       <ambientLight />
       <directionalLight position={[0, 0, 5]} intensity={2} />
       <OrbitControls />
